211421ec6d61b40be5c243e14d182fa4056273a3
刘剑 <liujian@magicare.me>
2019-03-14 10:24:39 +0800

    「remove」🚫删除dist

--- dist/constants/counter.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var GET_LIST_DATA = exports.GET_LIST_DATA = 'GET_LIST_DATA';
-var GET_USER_INFOS = exports.GET_USER_INFOS = 'GET_USER_INFOS';
--- dist/locales/project1.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = {
-  test: '项目A测试字段'
-};
